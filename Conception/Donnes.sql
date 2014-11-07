fc3eabd516e00fe5b93d38043679a22c2fdbf342
fa18swiss <jules.laville@gmail.com>
2014-11-07 13:12:46 +0000

    Ajout de données de test

--- Conception/Donnes.sql
+++ Conception/Donnes.sql
@@ -27,3 +27,10 @@ INSERT INTO file VALUES
 	(1, 1, 'Access file 1', 0x0),
 	(2, 1, 'Access file 2', 0x0),
 	(3, 2, 'Excel file 1', 0x0);
+
+
+UPDATE editors SET name = "Microsoft" WHERE id = 1;
+UPDATE programs SET editor_id = 3, name = "ISIC program 1" WHERE id = 5;
+INSERT INTO programs VALUES
+	(6, 1, NULL, 1, 'Exchange'),
+	(7, 2, NULL, 1, 'Node32'); 
