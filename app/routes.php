adea8b0597386ba9a29ee1a0ab0822bcdbab0c49
fa18swiss <jules.laville@gmail.com>
2014-11-17 09:36:10 +0000

    Route par défaut : UI

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,7 @@
 |
 */
 
-	Route::get('/', 'HomeController@showWelcome');
+	Route::get('/', 'HomeController@showUI');
     Route::get('/login', 'UserController@showLogin');  
 
     Route::controller("TreeView", "TreeViewController");
