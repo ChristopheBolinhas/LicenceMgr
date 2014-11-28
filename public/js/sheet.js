4eeaf1d37dd8bc9e845bd30b9d83c8d7e76a2ecb
fa18swiss <jules.laville@gmail.com>
2014-11-28 12:30:50 +0000

    Correction bug sur dropdown

--- public/js/sheet.js
+++ public/js/sheet.js
@@ -1,21 +1,18 @@
 function getFile(id) {
     location.href = "/sheet/get/" + id;    
 }
-// Explication de l'utilisation de ce principe dans le fichier view/sheet/list.blade.php
-function editSheet(that) {
-    console.log("editSheet");
-}
-function deleteSheet(that) {
-    var tr = getTr(that);
-    deleteItem(tr, '/sheet/delete/');
-}
 $(function() {
     var modal = $("#mainModal");
+    modal.on("click", ".deleteSheet", function() {
+        console.log("deleteSheet");
+        var tr = getTr(this);
+        deleteItem(tr, '/sheet/delete/');
+    });
     modal.on("click", ".getSheet", function() {
         console.log("getSheet");
         var id = getId(getTr(this));
         getFile(id);
     });
-    modal.on("click", ".editSheet", editSheet);
-    modal.on("click", ".deleteSheet", deleteSheet);
+    //modal.on("click", ".deleteSheet", deleteSheet);
+    
 });
