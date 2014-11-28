6357cba41a8967a29c1b9e04c5e608259de0fab7
fa18swiss <jules.laville@gmail.com>
2014-11-28 08:02:33 +0000

    Coté serveur pour edit

--- app/controllers/SheetController.php
+++ app/controllers/SheetController.php
@@ -9,6 +9,15 @@ class SheetController extends BaseController {
         return Response::download("/home/action/workspace/LicenceMgr/public/img/editor.png");
     }
     public function postEdit($id) {
+        $sheet = Sheet::findOrFail($id);        
+        switch (Input::get('key')) {
+            case "name":
+                $sheet->name = Input::get('value');
+                break;
+            default:
+                App::abort(404);
+        }
+        $sheet->save();        
         return Response::json(true);
     }
     public function deleteDelete($id) {
