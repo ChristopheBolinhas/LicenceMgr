888570e0acfaefba901c2c9fc10a41656188e8af
fa18swiss <jules.laville@gmail.com>
2014-12-06 15:59:01 +0000

    issue #31 amélioration des droit en autorisant la modif que sur les licences de l'entreprise

--- app/controllers/LicenceController.php
+++ app/controllers/LicenceController.php
@@ -14,25 +14,31 @@ class LicenceController extends BaseController {
             //$licences = Program::find($values[1])->licences;
             $licences = Licence::
                 where("program_id", "=", $values[1])
-                //->where("commpany_id", "=", 1)
+                ->where("company_id", "=", Auth::user()->company_id)
                 ->orderBy("name")
                 ->get()
                 ;   
             return View::make("licence/list")->with('licences',$licences);
+        } else {
+            App::abort(403);
         }
     }
     public function getKey($id) {
         if(Auth::user()->IsReadOrWrite() || Auth::user()->IsAdmin())
         {
             $licence = Licence::find($id);
-
-            return Response::json(array($licence->value));
+            if ($licence->company_id == Auth::user()->company_id) {
+                return Response::json(array($licence->value));
+            }
         }
+        App::abort(403);
     }
     public function deleteDelete($id) {
         if(Auth::user()->IsWrite() || Auth::user()->IsAdmin())
         {
             Licence::destroy($id);
+        } else {
+            App::abort(403);
         }
     }
     //Makes the edit licence modal appear to add a licence
@@ -46,6 +52,8 @@ class LicenceController extends BaseController {
                 ->with("idParent", $idParent)
                 ->with("cmd", "cmdAddLicence")
                 ;
+        } else {
+            App::abort(403);
         }
     }
     //Function to add a licence from the modal
@@ -62,6 +70,8 @@ class LicenceController extends BaseController {
             $ob->company_id = $user->company_id;
             $ob->created_user_id = $user->id;
             $this->fill($ob);
+        } else {
+            App::abort(403);
         }
     }
     //Makes the edit modal to modify a current licence
@@ -70,6 +80,11 @@ class LicenceController extends BaseController {
         {
             $licence = Licence::findOrFail($id);
 
+            if ($licence->company_id != Auth::user()->company_id) {
+                App::abort(403);
+                return;
+            }
+
             return View::make("licence/edit")
                 ->with("licence", $licence)
                 ->with("title", Lang::get('messages.modifyLicenceModalTitle'))
@@ -77,13 +92,23 @@ class LicenceController extends BaseController {
                 ->with("idParent", "")
                 ->with("cmd", "cmdEditLicence")
                 ;
+        } else {
+            App::abort(403);
         }
     }
     public function postEdit() {
         if(Auth::user()->IsWrite() || Auth::user()->IsAdmin())
         {
             $ob = Licence::findOrFail(Input::get('id'));
+            
+            if ($ob->company_id != Auth::user()->company_id) {
+                App::abort(403);
+                return;
+            }
+            
             $this->fill($ob);        
+        } else {
+            App::abort(403);
         }
     }
     private function fill($ob) {
@@ -93,6 +118,8 @@ class LicenceController extends BaseController {
             $ob->value = Input::get('value');
             $ob->updated_user_id = Auth::user()->id;
             $ob->save();
+        } else {
+            App::abort(403);
         }
     }
 }
