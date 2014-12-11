ad6a16b57327a072218da0971c0993656184ff64
fa18swiss <jules.laville@gmail.com>
2014-12-11 13:43:39 +0000

    Correction sintaxe

--- Conception/Donnes.sql
+++ Conception/Donnes.sql
@@ -46,8 +46,8 @@ GRANT ALL PRIVILEGES ON LicMgr.* TO 'licmgr'@'localhost' WITH GRANT OPTION;
 
 INSERT INTO `users` VALUES
     (1,1,'Super Admin','superadmin','$2y$10$HDX6ZIcR/MLmO92L0xfevutqcyBtdfOrvinVflOYuAWtidiWn1HpG','superadmin@he-arc.ch','0','2014-11-18 11:49:07','2014-12-06 15:21:27'),
-    (2,1,'Admin HE-ARC','hearc','$2y$10$qAZy1PNTGvWDsUDO8XhxVuM63YBXe.yJl7Iw\/KuBx5FO0N7QJTeTi','admin@he-arc.ch','0','2014-11-18 11:49:07','2014-12-06 15:21:27');
-    (3,1,'Writer HE-ARC','writer','$2y$10$Nd8iAYKLX2PlUhG6kcLrseqAYM\/vw\/4hDeH\/8ING3hts3C.znVi\/m','writer@he-arc.ch','0','2014-11-18 11:49:07','2014-12-06 15:21:27');
+    (2,1,'Admin HE-ARC','hearc','$2y$10$qAZy1PNTGvWDsUDO8XhxVuM63YBXe.yJl7Iw\/KuBx5FO0N7QJTeTi','admin@he-arc.ch','0','2014-11-18 11:49:07','2014-12-06 15:21:27'),
+    (3,1,'Writer HE-ARC','writer','$2y$10$Nd8iAYKLX2PlUhG6kcLrseqAYM\/vw\/4hDeH\/8ING3hts3C.znVi\/m','writer@he-arc.ch','0','2014-11-18 11:49:07','2014-12-06 15:21:27'),
     (4,1,'Reader HE-ARC','reader','$2y$10$Yq2zAf\/SYUH2dH58wEq.Ke\/hYRW1sgziXXHjdkApfKWZZmyv74ZM.','reader@he-arc.ch','0','2014-11-18 11:49:07','2014-12-06 15:21:27');
 
 INSERT INTO role_user VALUES
