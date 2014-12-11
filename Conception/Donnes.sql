66923d776e8b88c0a3f8958d761c4f98c8f59d8d
fa18swiss <jules.laville@gmail.com>
2014-12-11 13:39:13 +0000

    MAJ des scripts de création

--- Conception/Donnes.sql
+++ Conception/Donnes.sql
@@ -41,12 +41,23 @@ INSERT INTO licences VALUES
 	(3, 3, 1, 'Access he-arc 2', 'AAAAA-XXXXX-XXXXX-XXXXX-XXXXX', 0, now(), 0, now()),
 	(4, 4, 1, 'Excel he-arc 2', 'BBBBB-XXXXX-XXXXX-XXXXX', 0, now(), 0, now());
 
-ALTER TABLE licences CHANGE update_at updated_at datetime;
+CREATE USER 'licmgr'@'localhost' IDENTIFIED BY 'licmgr';
+GRANT ALL PRIVILEGES ON LicMgr.* TO 'licmgr'@'localhost' WITH GRANT OPTION;
 
-RENAME TABLE user_roles TO user_role;
+INSERT INTO `users` VALUES
+    (1,1,'Super Admin','superadmin','$2y$10$HDX6ZIcR/MLmO92L0xfevutqcyBtdfOrvinVflOYuAWtidiWn1HpG','superadmin@he-arc.ch','0','2014-11-18 11:49:07','2014-12-06 15:21:27'),
+    (2,1,'Admin HE-ARC','hearc','$2y$10$qAZy1PNTGvWDsUDO8XhxVuM63YBXe.yJl7Iw\/KuBx5FO0N7QJTeTi','admin@he-arc.ch','0','2014-11-18 11:49:07','2014-12-06 15:21:27');
+    (3,1,'Writer HE-ARC','writer','$2y$10$Nd8iAYKLX2PlUhG6kcLrseqAYM\/vw\/4hDeH\/8ING3hts3C.znVi\/m','writer@he-arc.ch','0','2014-11-18 11:49:07','2014-12-06 15:21:27');
+    (4,1,'Reader HE-ARC','reader','$2y$10$Yq2zAf\/SYUH2dH58wEq.Ke\/hYRW1sgziXXHjdkApfKWZZmyv74ZM.','reader@he-arc.ch','0','2014-11-18 11:49:07','2014-12-06 15:21:27');
 
-INSERT INTO user_role VALUES
+INSERT INTO role_user VALUES
     (1, 1),
     (1, 2),
     (1, 3),
-    (1, 4);
+    (1, 4),
+    (2, 2),
+    (2, 3),
+    (2, 4),
+    (3, 3),
+    (3, 4),
+    (4, 4);
